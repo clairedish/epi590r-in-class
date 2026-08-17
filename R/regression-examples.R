89b844cb6e74ccd2b1e0fcde152583d102c329d5
Claire Dishman <claire.dishman@emory.edu>
2026-08-17 16:23:43 -0400

    Regression excercises

--- R/regression-examples.R
+++ R/regression-examples.R
@@ -132,3 +132,51 @@ tbl_merge(list(tbl_no_int, tbl_int),
 )
 
 
+# Each of the univariate regression examples held the outcome (y =) constant,
+# while varying the predictor variables with include =. You can also look at one
+# predictor across several outcomes. Create a univariate regression table
+# looking at the association between sex (sex_cat) as the x = variable and each
+# of nsibs, sleep_wkdy, and sleep_wknd, and income.
+tbl_uvregression(
+	nlsy,
+	x = sex_cat,
+	include = c(
+		nsibs, sleep_wkdy,
+		sleep_wknd, income
+	),
+	method = lm
+)
+
+# Fit a Poisson regression (family = poisson()) for the number of siblings,
+# using at least 3 predictors of your choice. Create a nice table displaying
+# your Poisson regression and its exponentiated coefficients.
+poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat + income,
+											data = nlsy, family = poisson()
+)
+
+tbl_regression(
+	poisson_model,
+	exponentiate = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		eyesight_cat ~ "Eyesight",
+		income ~ "Income (USD)"
+	)
+)
+
+# Instead of odds ratios for wearing glasses, as in the example in the slides.,
+# we want risk ratios. We can do this by specifying in the regression family =
+# binomial(link = "log"). Regress glasses on eyesight_cat and sex_cat and create
+# a table showing the risk ratios and confidence intervals from this regression.
+logbinomial_model <- glm(glasses ~ eyesight_cat + sex_cat,
+											data = nlsy, family = binomial(link = "log")
+)
+
+logbinomial_table <- tbl_regression(
+	logbinomial_model,
+	intercept = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		sex_cat ~ "Sex",
+	)
+)
