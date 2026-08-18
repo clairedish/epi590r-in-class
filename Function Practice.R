8eeb65d9828faff03c64dcce237a03910646f0eb
Claire Dishman <claire.dishman@emory.edu>
2026-08-18 14:04:46 -0400

    funtion one

---
+++ Function Practice.R
@@ -0,0 +1,13 @@
+# start out with a number to test
+x <- 3
+# you'll want your function to return this number
+x^2
+square <- function(x) {
+	square_val <- x^2
+	return(square_val)
+}
+# test it out
+square(x)
+square(53)
+53^2 # does this match?
+
