60bfb132726596de0748dd6c88e08f188a845478
Claire Dishman <claire.dishman@emory.edu>
2026-08-18 15:47:27 -0400

    std funtions

--- Function Practice.R
+++ Function Practice.R
@@ -27,3 +27,52 @@ raise <- function(x, power=2) {
 	return(raised_val)
 }
 raise(5)
+
+
+x <- c(1,15,44,3,9,100,59,45)
+
+
+std <- function(x, na.rm = TRUE) {
+	n <- length(x)
+	xmean <- sum(x) / n
+	variance <- x - xmean
+	varsq <- variance^2
+	sum_of_squares <- sum(varsq)
+	n_minus_1 <- length(x) -1
+	stdval <- sqrt(sum_of_squares / n_minus_1)
+	if (n <= 1) {
+		stdval <- NA
+	} else {
+		stdval <- stdval
+	}
+	return(stdval)
+}
+
+std(x)
+
+#check for na and remove them
+std <- function(x, na.rm = TRUE) {
+	if (na.rm) {
+		new_x <-	na.omit(x)
+	} else {
+		new_x <- x
+	}
+	if (length(new_x)<= 1) {
+		stdval <- NA
+	} else {
+		n <- length(new_x)
+		xmean <- sum(new_x) / n
+		variance <- new_x - xmean
+		varsq <- variance^2
+		sum_of_squares <- sum(varsq)
+		n_minus_1 <- length(new_x) -1
+		stdval <- sqrt(sum_of_squares / n_minus_1)
+		stdval <- stdval
+	}
+	return(stdval)
+}
+sd(x)
+
+std(3)
+
+
